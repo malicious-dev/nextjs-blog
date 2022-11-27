a6db686c82673c344bd6375d4c82f262189f29ad
malwareami! <46420533+malicious-dev@users.noreply.github.com>
2022-11-28 01:58:20 +0530

    ui and saver started

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,96 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import CheckIcon from '@mui/icons-material/Check';
+import DeleteIcon from '@mui/icons-material/Delete';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import EditIcon from '@mui/icons-material/Edit';
 
 export default function Home() {
+
+  const [checked, setChecked] = React.useState([0]);
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    console.log(currentIndex)
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
+
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Welcome to new world</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-    </Layout>
+    <>
+    <React.Fragment>
+      <CssBaseline />
+      <Container className="container">
+        <h1>TODO MERN</h1> 
+      <TextField id="standard-basic" label="Enter TODO" variant="standard" />
+
+        <IconButton color="success" aria-label="upload picture" component="label">
+
+        <CheckIcon />
+      </IconButton>
+    
+
+<List sx={{ bgcolor: 'background.paper' }}>
+      {[0, 1, 2, 3].map((value) => {
+        const labelId = `checkbox-list-label-${value}`;
+
+        return (
+          <ListItem
+            key={value}
+            secondaryAction={
+              <><IconButton color="primary" aria-label="upload picture" component="label">
+
+                <EditIcon />
+
+              </IconButton><IconButton color="error" aria-label="upload picture" component="label">
+
+                  <DeleteIcon />
+
+                </IconButton></>
+            }
+            disablePadding
+          >
+            <ListItemButton role={undefined} onClick={handleToggle(value)} dense>
+              <ListItemIcon>
+                <Checkbox
+                  edge="start"
+                  checked={checked.indexOf(value) !== -1}
+                  tabIndex={-1}
+                  disableRipple
+                  inputProps={{ 'aria-labelledby': labelId }}
+                />
+              </ListItemIcon>
+              <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+            </ListItemButton>
+          </ListItem>
+        );
+      })}
+    </List>
+
+      </Container>
+    </React.Fragment>
+   
+    </>
   );
 }
---
+++ server/server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+var port = process.env.PORT || 8000;;
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send("Welcome!!")
+})
+
+
+app.listen(port, ()=>{
+  console.log(`server is started at ${port}`);
+})
